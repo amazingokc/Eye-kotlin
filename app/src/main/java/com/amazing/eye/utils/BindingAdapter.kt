1bc3c8f41339ee0055532a4854f602520f35e408
amazingokc <408188680@qq.com>
2020-01-02 10:54:41 +0800

    评论列表头像添加默认头

--- app/src/main/java/com/amazing/eye/utils/BindingAdapter.kt
+++ app/src/main/java/com/amazing/eye/utils/BindingAdapter.kt
@@ -1,8 +1,12 @@
 package com.amazing.eye.utils
 
 import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
 import androidx.databinding.BindingAdapter
 import android.widget.ImageView
+import androidx.constraintlayout.widget.Placeholder
+import com.amazing.eye.ApplicationContext
+import com.amazing.eye.R
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -14,9 +18,9 @@ fun loadNormalImage(
     ImageLoaderUtil.getInstance().loadNormalImg(imageView, url, null, null)
 }
 
-@BindingAdapter("imgUrlCircleUrl")
-fun loadCircleImage(imageView: ImageView, url: String) {
-    ImageLoaderUtil.getInstance().loadCircleImg(imageView, url, null, null)
+@BindingAdapter("imgUrlCircleUrl", "placeholder", "error")
+fun loadCircleImage(imageView: ImageView, url: String, placeholder: Drawable, error: Drawable) {
+    ImageLoaderUtil.getInstance().loadCircleImg(imageView, url, placeholder, error)
 }
 
 fun String.getTimeWithDuration(duration: Long): String {
